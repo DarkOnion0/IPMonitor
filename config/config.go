88de21127b0becc62707db8a84ae436a7e05fdd5
DarkOnion0 <darkonion0@clapot.org>
2022-07-06 16:08:40 +0200

    refactor: streamline the cron setting accross the CLI and the container

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ func init() {
 
 var (
 	Debug      = flag.String("debug", "false", "Sets log level to debug")
-	Cron       = flag.String("cron", "*/15 * * * *", "Set a custom cron scheduled to run the IP check, run every 15 minutes by default")
+	Cron       = flag.String("cron", "*/10 * * * *", "Set a custom cron scheduled to run the IP check, run every 15 minutes by default")
 	EnableCron = flag.String("cron-enable", "true", "This flag enable the cron mode, it can be disable to it in API mode only")
 	EnableAPI  = flag.String("api-enable", "true", "This flag enable the API mode, it can be disable to run it in cron mode only")
 	APIPort    = flag.String("api-port", "8080", "Set a custom api's listen port")
